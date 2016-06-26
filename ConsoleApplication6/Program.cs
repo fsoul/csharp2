697a5b84edcf8bb7cf06663d818976e5d1c13b09
fsoul <bilinskyivitalii@gmail.com>
2016-06-26 17:23:38 +0300

    update

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication6
 {
@@ -10,6 +6,12 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
+            Console.Write("Введи число: ");
+            int i = int.Parse(Console.ReadLine());
+            for (int a = 1; a <= i; a++)
+            {
+                if (i % a == 0) Console.Write("{0} ", a);
+            }
         }
     }
 }
